664010bf287c1bce0d91741cd2b8331ee0f19cf6
Jake-NSW <55055481+Jake-NSW@users.noreply.github.com>
2021-12-20 17:39:49 +1100

    Don't fetch commands twice when entering playmode

--- Assets/Espionage.Engine/Runtime/Code/Utility/Managers/Manager.cs
+++ Assets/Espionage.Engine/Runtime/Code/Utility/Managers/Manager.cs
@@ -7,32 +7,32 @@ namespace Espionage.Engine.Internal
 {
 	internal sealed class Manager
 	{
-		[RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.AfterAssembliesLoaded )]
-		private static void InvokeRuntime()
-		{
-			Invoker( Layer.Runtime );
-		}
-
-#if UNITY_EDITOR
-
-		[UnityEditor.InitializeOnLoadMethod]
-		private static void InvokeEditor()
-		{
-			Invoker( Layer.Editor );
-		}
-
-#endif
-
 		private static void Invoker( Layer layer )
 		{
 			// Get all manager types
 			var types = AppDomain.CurrentDomain.GetAssemblies()
 								.SelectMany( e => e.GetTypes()
-								.Where( e => e.IsDefined( typeof( ManagerAttribute ), false ) && e.GetCustomAttribute<ManagerAttribute>().Layer.HasFlag( layer ) ) );
+								.Where( e => e.IsDefined( typeof( ManagerAttribute ), false ) && e.GetCustomAttribute<ManagerAttribute>().Layer == layer ) );
 
 
 			foreach ( var item in types )
 				item.GetMethod( item.GetCustomAttribute<ManagerAttribute>().Method, BindingFlags.Static | BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic )?.Invoke( null, null );
 		}
+
+		//
+		// Runtime
+		//
+
+		[RuntimeInitializeOnLoadMethod( RuntimeInitializeLoadType.AfterAssembliesLoaded )]
+		private static void InvokeRuntime() => Invoker( Layer.Runtime );
+
+		//
+		// Editor
+		//
+
+#if UNITY_EDITOR
+		[UnityEditor.InitializeOnLoadMethod]
+		private static void InvokeEditor() => Invoker( Layer.Editor );
+#endif
 	}
 }
