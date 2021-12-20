8386e128e16de81f65b6148af4c29ccff5995e27
Jake-NSW <55055481+Jake-NSW@users.noreply.github.com>
2021-12-21 10:37:49 +1100

    Update Event.cs

--- Assets/Espionage.Engine/Runtime/Code/Utility/Events/Event.cs
+++ Assets/Espionage.Engine/Runtime/Code/Utility/Events/Event.cs
@@ -1,9 +1,21 @@
-using System.Collections;
 // Attribute based event callback system
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Espionage.Engine
 {
 	public static class Event
 	{
+		internal struct EventCallback
+		{
+			public Type Class { get; set; }
+			public MethodInfo Info { get; set; }
+		}
+
+		public static void Register( object item )
+		{
+
+		}
 	}
 }
