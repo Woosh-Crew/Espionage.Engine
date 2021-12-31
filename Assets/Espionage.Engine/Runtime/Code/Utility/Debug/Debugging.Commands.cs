d5560611edf3db045d0d4e0651a80706136dd7ab
Jake-NSW <55055481+Jake-NSW@users.noreply.github.com>
2021-12-31 22:27:12 +1100

    Readd back default commands

--- Assets/Espionage.Engine/Runtime/Code/Utility/Debug/Debugging.Commands.cs
+++ Assets/Espionage.Engine/Runtime/Code/Utility/Debug/Debugging.Commands.cs
@@ -7,6 +7,27 @@ namespace Espionage.Engine
 {
 	public static partial class Debugging
 	{
+		[Debugging.Cmd( "clear", "cls" )]
+		private static void ClearCmd()
+		{
+			Log.Clear();
+		}
 
+		[Debugging.Cmd( "help", "?" )]
+		private static void HelpCmd()
+		{
+			Log.Info( "All Commands" );
+
+			foreach ( var item in Console.All )
+			{
+				Log.Info( $"{item.Name} - {item.Help}" );
+			}
+		}
+
+		[Debugging.Cmd( "quit", "exit" )]
+		private static void QuitCmd()
+		{
+			Log.Error( "This should probably quit the game" );
+		}
 	}
 }
