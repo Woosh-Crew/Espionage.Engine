9a687f4fe85dbb3e179d9fe19554113ff1b4976b
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-20 20:23:25 +1100

    Update Utility.cs

--- Base/Runtime/Utility/Utility.cs
+++ Base/Runtime/Utility/Utility.cs
@@ -6,31 +6,11 @@ namespace Espionage.Engine.Internal
 {
 	public static class Utility
 	{
-		private static readonly string[] IgnoredAssemblies = new string[]
-		{
-			"Unity",
-			"System",
-			"mscorlib",
-			"netstandard",
-			"Facepunch.Steamworks",
-			"ExCSS",
-			"Bee.BeeDriver",
-			"nunit.framework",
-			"PlayerBuildProgram",
-			"Mono",
-			"JetBrains",
-			"Report"
-		};
+		private static readonly string[] IgnoredAssemblies = { "Unity", "System", "mscorlib", "netstandard", "Facepunch.Steamworks", "ExCSS", "Bee.BeeDriver", "nunit.framework", "PlayerBuildProgram", "Mono", "JetBrains", "Report" };
 
 		public static bool IgnoreIfNotUserGeneratedAssembly( Assembly assembly )
 		{
-			if ( IgnoredAssemblies.All( item => !assembly.FullName.StartsWith( item ) ) )
-			{
-				Debugging.Log.Info( assembly.FullName );
-				return true;
-			}
-
-			return false;
+			return IgnoredAssemblies.All( item => !assembly.FullName.StartsWith( item ) );
 		}
 	}
 }
