ff7183fff7409495ea6f98de78d1a26a647816d6
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-22 15:48:19 +1100

    Map Asset Importer

---
+++ Resource/Editor/Importer/Map.Importer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+namespace Espionage.Engine.Resources.Editor
+{
+	[ScriptedImporter(1, "map")]
+    public class MapImporter : ScriptedImporter
+    {
+	    public override void OnImportAsset( AssetImportContext ctx )
+        {
+        }
+    }
+}
