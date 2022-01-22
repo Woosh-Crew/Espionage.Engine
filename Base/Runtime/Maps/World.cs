0f23abc952afa1d3b6ab5b8f2f6c3b5bae8e47e5
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-22 22:32:15 +1100

    Add World, Updates to Map and Sound, IResource Load & Unload must return bool

---
+++ Base/Runtime/Maps/World.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Espionage.Engine
+{
+    public abstract class World : MonoBehaviour
+    {
+	    public string title;
+	    public string description;
+    }
+}
