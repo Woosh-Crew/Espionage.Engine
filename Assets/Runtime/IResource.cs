742737cd77e239ed5ae861fc81cf590c39f08c84
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-22 13:56:07 +1100

    Updates

---
+++ Assets/Runtime/IResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Espionage.Engine.Assets
+{
+	public interface IResource
+	{
+		string Path { get; }
+		bool IsLoading { get; }
+		AssetBundle Bundle { get; }
+		
+		
+		void Load( Action onLoad = null  );
+		void Unload( Action onUnload = null );
+	}
+}
---
+++ Base/Runtime/Utility/Unity/Extensions/Scene.Extensions.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Espionage.Engine
+{
+	public static class SceneExtensions
+	{
+		/// <summary>
+		/// Instantiate a GameObject and place it in this Scene.
+		/// </summary>
+		/// <param name="self">A Scene instance.</param>
+		/// <param name="original">An existing object that you want to make a copy of.</param>
+		/// <returns>The instantiated GameObject.</returns>
+		public static GameObject Instantiate( this Scene self, GameObject original )
+		{
+			var o = Object.Instantiate( original );
+
+			if ( !self.isLoaded || !self.IsValid() )
+			{
+				Debugging.Log.Warning( "Scene wasn't valid" );
+				return o;
+			}
+
+			if ( o == null )
+			{
+				return null;
+			}
+
+			SceneManager.MoveGameObjectToScene( o, self );
+			return o;
+		}
+
+		/// <summary>
+		/// Unload this Scene.
+		/// </summary>
+		/// <param name="self">A Scene instance.</param>
+		public static void Unload( this Scene self )
+		{
+			if ( self.isLoaded && self.IsValid() )
+			{
+				SceneManager.UnloadSceneAsync( self.name );
+			}
+		}
+
+		/// <summary>
+		/// Get a Component of Type T in this Scene. Returns the first found Component.
+		/// </summary>
+		/// <typeparam name="T">A Type that derives from Component</typeparam>
+		/// <param name="self">A Scene instance.</param>
+		/// <returns>A Component of Type T or null if none is found.</returns>
+		public static T GetComponentInScene<T>( this Scene self )
+		{
+			if ( !self.isLoaded || !self.IsValid() )
+			{
+				return default;
+			}
+
+			foreach ( var go in self.GetRootGameObjects() )
+			{
+				var component = go.GetComponentInChildren<T>( true );
+				if ( component != null )
+				{
+					return component;
+				}
+			}
+
+			return default;
+		}
+	}
+}
