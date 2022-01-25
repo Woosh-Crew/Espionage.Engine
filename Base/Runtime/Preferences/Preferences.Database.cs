f73fd88c334b40740f1a5f33dff462e8e784e888
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-26 09:43:35 +1100

    Work on preferences system, Cache all properties on class for library.

---
+++ Base/Runtime/Preferences/Preferences.Database.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Espionage.Engine
+{
+	public static partial class Preferences
+	{
+		private class InternalDatabase : IDatabase<Item, string>
+		{
+			public IEnumerable<Item> All => _records.Values;
+			private readonly Dictionary<string, Item> _records = new();
+
+			public Item this[ string key ] => _records[key];
+
+			public void Add( Item item )
+			{
+				_records.Add( item.Name!, item );
+			}
+
+			public void Clear()
+			{
+				_records.Clear();
+			}
+
+			public bool Contains( Item item )
+			{
+				return _records.ContainsKey( item.Name );
+			}
+
+			public void Remove( Item item )
+			{
+				_records.Remove( item.Name );
+			}
+		}
+	}
+}
