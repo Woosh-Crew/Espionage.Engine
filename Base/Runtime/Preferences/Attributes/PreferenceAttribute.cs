9884c08c05265a1c8016aff621a4fda806acd0c8
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-26 09:04:38 +1100

    Start work on attribute based preferences system.

---
+++ Base/Runtime/Preferences/Attributes/PreferenceAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Espionage.Engine
+{
+	[AttributeUsage( AttributeTargets.Property )]
+	public class PreferenceAttribute : Attribute
+	{
+		public string Title { get; set; }
+		public string Tooltip { get; set; }
+	}
+}
