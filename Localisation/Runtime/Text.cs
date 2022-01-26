fda2357d27305b22c3de97947906a412cbba6819
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-27 09:11:36 +1100

    Create basic Text class

--- Localisation/Runtime/Text.cs
+++ Localisation/Runtime/Text.cs
@@ -9,5 +9,18 @@ namespace Espionage.Engine.Languages
 	/// string, you feed it the Id of a localised string, and it
 	/// will return a string. Very helpful!
 	/// </summary>
-	public class Text { }
+	public class Text
+	{
+		private string _id;
+
+		public Text( string id )
+		{
+			_id = id;
+		}
+
+		public static implicit operator string( Text text )
+		{
+			return Localisation.Current.Localisation[text._id];
+		}
+	}
 }
