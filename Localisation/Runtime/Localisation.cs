516a83693a9f13f18c6638320fade2cfb48a5bdd
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-27 08:19:11 +1100

    Start on Localisation Module

---
+++ Localisation/Runtime/Localisation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Espionage.Engine.Language
+{
+	public static class Localisation 
+	{
+
+	}
+}
---
+++ Localisation/Runtime/Text.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Espionage.Engine.Language
+{
+	public class Text
+	{
+	}
+}
