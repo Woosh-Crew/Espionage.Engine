4bf892acea551f0a06087bfe042ed7b8d6d6ec88
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-28 08:49:18 +1100

    Add internal camera controller for using the tripod system

---
+++ Base/Runtime/Engine/Camera/Internal/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Espionage.Engine
+{
+	[RequireComponent( typeof( Camera ) )]
+	internal class CameraController : SingletonComponent<CameraController>
+	{
+		private Camera _target;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			_target = GetComponent<Camera>();
+		}
+
+		public void Finalise( Tripod.Setup camSetup )
+		{
+			var trans = transform;
+			trans.position = camSetup.Position;
+			trans.rotation = camSetup.Rotation;
+		}
+	}
+}
