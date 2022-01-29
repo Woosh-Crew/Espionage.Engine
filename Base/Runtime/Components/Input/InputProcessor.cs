9972723b286b68a29f3e951617c82452ceee5f65
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-29 18:07:37 +1100

    Add InputProcessor and Pawn

---
+++ Base/Runtime/Components/Input/InputProcessor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Espionage.Engine
+{
+    public class InputProcessor : MonoBehaviour
+    {
+		private void Update()
+        {
+			// Process Input	
+        }
+    }
+}
