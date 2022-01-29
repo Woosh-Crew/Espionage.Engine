0abfb94ba116440e32bcbbe51e47092f4b5499bc
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-29 13:21:46 +1100

    Psuedo code for renderer

--- Base/Runtime/Entity/Components/Renderer.cs
+++ Base/Runtime/Entity/Components/Renderer.cs
@@ -1,7 +1,15 @@
 ﻿namespace Espionage.Engine.Components
 {
+	/// <summary>
+	/// A Simple Component that can get attached to entities
+	/// to render objects. Under the hood it just spawns a GameObject
+	/// with the respective Model
+	/// </summary>
 	public class Renderer : IComponent<Entity>
 	{
+		// Model
+		// Transform
+
 		public void OnAttached( Entity item )
 		{
 			// Spawn Model GameObject from Assets
@@ -10,6 +18,6 @@
 		public void OnDetached( Entity item )
 		{
 			// Delete GameObject
-		}	
+		}
 	}
 }
