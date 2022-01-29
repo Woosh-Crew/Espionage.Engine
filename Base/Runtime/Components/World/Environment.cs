1cfb75022839e3306c574452d79d7acaf17517c6
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-29 20:21:14 +1100

    Documentation for environment

--- Base/Runtime/Components/World/Environment.cs
+++ Base/Runtime/Components/World/Environment.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 namespace Espionage.Engine.World
 {
 	/// <summary>
-	/// Environment generates world lighting
+	/// Environment generates world lighting. This includes
+	/// the Directional light, skybox, fog, Post Processing, etc.
 	/// </summary>
 	[Group( "Maps.Lighting" )]
 	public class Environment : ScriptableObject
