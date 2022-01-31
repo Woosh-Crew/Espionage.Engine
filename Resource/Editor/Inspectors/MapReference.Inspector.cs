8a6600bad1bb4a75ee9aa08d4d0d61a9ae02aec2
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-31 22:46:38 +1100

    Update Map Reference inspector UI

--- Resource/Editor/Inspectors/MapReference.Inspector.cs
+++ Resource/Editor/Inspectors/MapReference.Inspector.cs
@@ -15,73 +15,105 @@ namespace Espionage.Engine.Resources.Editor
 		// Properties
 		//
 
+		// Meta
 		private SerializedProperty _title;
 		private SerializedProperty _description;
 		private SerializedProperty _icon;
-		
+
+		// Build
+		private SerializedProperty _sceneAsset;
+		private SerializedProperty _buildOptions;
+
 		private void OnEnable()
 		{
 			_title = serializedObject.FindProperty( "title" );
 			_description = serializedObject.FindProperty( "description" );
 			_icon = serializedObject.FindProperty( "icon" );
+
+			_sceneAsset = serializedObject.FindProperty( "sceneAsset" );
+			_buildOptions = serializedObject.FindProperty( "buildOptions" );
 		}
-		
+
 		//
 		// GUI
 		//
 
+		public override bool UseDefaultMargins()
+		{
+			return false;
+		}
+
 		public override void OnInspectorGUI()
 		{
-			OnInfoGUI();
-			
-			GUILayout.Space(8);
-			
-			OnCompileGUI();
+			using ( _ = new EditorGUILayout.VerticalScope( Styles.Container ) )
+			{
+				using ( _ = new EditorGUILayout.VerticalScope( Styles.Group ) )
+				{
+					OnInfoGUI();
+				}
 
-			base.OnInspectorGUI();
+				GUILayout.Space( 8 );
+
+				using ( _ = new EditorGUILayout.VerticalScope( Styles.Group ) )
+				{
+					OnCompileGUI();
+				}
+			}
 		}
 
 		private void OnInfoGUI()
 		{
 			serializedObject.Update();
-			
+
+			EditorGUILayout.LabelField( "Meta Data", EditorStyles.boldLabel );
+			GUILayout.Space( 2 );
+
 			// Properties
 			EditorGUILayout.BeginHorizontal();
 			{
-				EditorGUILayout.PropertyField( _icon );
-				
+				_icon.objectReferenceValue = (Texture2D)EditorGUILayout.ObjectField( _icon.objectReferenceValue, typeof( Texture2D ), false, GUILayout.Width( 92 ), GUILayout.Height( 90 ) );
+
 				EditorGUILayout.BeginVertical();
 				{
-					_title.stringValue = EditorGUILayout.TextField( _title.stringValue );
-					_description.stringValue = EditorGUILayout.TextArea( _description.stringValue, EditorStyles.textArea, GUILayout.Height(64) );
+					_title.stringValue = EditorGUILayout.TextField( _title.stringValue, Styles.TitleField, GUILayout.Height( 24 ) );
+					_description.stringValue = EditorGUILayout.TextArea( _description.stringValue, EditorStyles.textArea, GUILayout.Height( 64 ) );
 				}
 				EditorGUILayout.EndVertical();
 			}
 			EditorGUILayout.EndHorizontal();
 
 			serializedObject.ApplyModifiedProperties();
-			
-			
 		}
 
 		private void OnCompileGUI()
 		{
+			EditorGUILayout.LabelField( "Compiler", EditorStyles.boldLabel );
+			GUILayout.Space( 2 );
+
+			EditorGUILayout.PropertyField( _sceneAsset );
+			EditorGUILayout.PropertyField( _buildOptions );
+
+			GUILayout.Space( 4 );
+
 			using ( _ = new EditorGUI.DisabledGroupScope( !Map.CanCompile() ) )
 			{
-				if ( GUILayout.Button( "Compile", Styles.CompileButton, GUILayout.Height(28) ) )
+				if ( GUILayout.Button( "Compile", Styles.CompileButton, GUILayout.Height( 28 ) ) )
 				{
 					Map.Compile();
 				}
 			}
 		}
-		
+
 		//
 		// GUI Styles
 		//
-		
+
 		private static class Styles
 		{
-			public static readonly GUIStyle CompileButton = new GUIStyle( "Button" ) ;
+			public static readonly GUIStyle Container = new() { padding = new RectOffset( 12, 12, 12, 12 ) };
+			public static readonly GUIStyle Group = new( GUI.skin.window ) { padding = new RectOffset( 8, 8, 8, 8 ) };
+			public static readonly GUIStyle CompileButton = new( "Button" );
+			public static readonly GUIStyle TitleField = new( EditorStyles.textField ) { alignment = TextAnchor.MiddleLeft };
 		}
 	}
 }
