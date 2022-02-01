e2473752e23ae35bff5d22e6ad326e12385e3c34
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-01 12:03:31 +1100

    Create Sound

---
+++ Sound/Runtime/Assets/Sound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Espionage.Engine.Resources;
+using UnityEngine;
+
+namespace Espionage.Engine.Sound
+{
+	[CreateAssetMenu, File( Extension = "sfx" ), Group( "Sounds" )]
+    public class Sound : Asset
+    {
+
+    }
+}
