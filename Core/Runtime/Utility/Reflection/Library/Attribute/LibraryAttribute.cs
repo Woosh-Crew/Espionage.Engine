19274e3d73c3e1692bd4344888ef3621b543f79b
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-02 12:22:20 +1100

    Library and Local documentation pass

--- Core/Runtime/Utility/Reflection/Library/Attribute/LibraryAttribute.cs
+++ Core/Runtime/Utility/Reflection/Library/Attribute/LibraryAttribute.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Espionage.Engine
 {
+	/// <summary>
+	/// Add this attribute to a class to add it to the library database,
+	/// or to override its internal name for identification.
+	/// </summary>
 	[AttributeUsage( AttributeTargets.Class )]
 	public sealed class LibraryAttribute : Attribute
 	{
