6598d87801f0c5c4d192abb64330789a337cf9ef
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-10 10:10:42 +1100

    Remove Entity Definition and Database, Add Blueprint, and Asset Library Component

---
+++ Core/Runtime/Game/Blueprint/Blueprint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Espionage.Engine
+{
+	/// <summary>
+	/// Blueprints are used for spawning prefabs at runtime using C#
+	/// </summary>
+    public abstract class Blueprint : ILibrary
+    {
+	    public Library ClassInfo { get; }
+	    
+	    public Blueprint()
+	    {
+		    ClassInfo = Library.Database[GetType()];
+	    }
+
+	    public GameObject Spawn()
+	    {
+		    return null;
+	    }
+    }
+}
---
+++ Utility/Runtime/Reflection/Components/AssetAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Espionage.Engine.Components;
+
+namespace Espionage.Engine
+{
+	[AttributeUsage( AttributeTargets.Class )]
+	public class AssetAttribute : Attribute, IComponent<Library>
+	{
+		/// <summary>
+		/// The path to the asset.
+		/// </summary>
+		public string Path { get; set; }
+
+		public void OnAttached( Library library ) { }
+	}
+}
