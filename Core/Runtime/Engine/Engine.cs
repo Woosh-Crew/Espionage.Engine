92090dd774c67e0f76bd4413a717637a7ba3c3b8
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-10 12:04:37 +1100

    Create Loader Service

--- Core/Runtime/Engine/Engine.cs
+++ Core/Runtime/Engine/Engine.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Text;
 using Espionage.Engine.Services;
 using UnityEngine;
 using UnityEngine.LowLevel;
---
+++ Core/Runtime/Engine/Services/LoaderService.cs
@@ -0,0 +1,18 @@
+﻿namespace Espionage.Engine.Services
+{
+	public class LoaderService : IService
+	{
+		public Library ClassInfo { get; }
+
+		public LoaderService()
+		{
+			ClassInfo = Library.Database[GetType()];
+		}
+
+		public Loader Loader => Engine.Game.Loader;
+
+		public void OnReady() {  }
+		public void OnShutdown() { }
+		public void OnUpdate() {  }
+	}
+}
