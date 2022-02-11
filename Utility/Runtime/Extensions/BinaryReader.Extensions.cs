1f3f040d0a21680115cd37a871e5aecdaa9f172e
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-11 20:18:21 +1100

    Create BinaryReader.Extensions, Add ReadVec3

---
+++ Utility/Runtime/Extensions/BinaryReader.Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using UnityEngine;
+
+public static class BinaryReaderExtensions
+{
+	public static Vector3 ReadVec3( this BinaryReader reader )
+	{
+		var x = reader.ReadSingle();
+		var y = reader.ReadSingle();
+		var z = reader.ReadSingle();
+
+		return new Vector3( x, y, z );
+	}
+}
