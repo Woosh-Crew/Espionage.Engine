9d6b04c0fc583c7d8b7f2229ab3d4df4d00a642b
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-16 18:30:12 +1100

    Update Spawn Point

--- Core/Runtime/Game/Behaviours/World/SpawnPoint.cs
+++ Core/Runtime/Game/Behaviours/World/SpawnPoint.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 
 namespace Espionage.Engine
 {
-	[Library("info.player_start")]
-	public class SpawnPoint : Behaviour { }
+	[Library( "info.player_start" )]
+	public class SpawnPoint : Behaviour
+	{
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawWireSphere( transform.position, 0.25f );
+			Gizmos.color = Color.white;
+		}
+	}
 }
