f17d6e7d7e93af1b5b7cce327255598d75dacb05
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-16 17:12:39 +1100

    Incredibly simple FPS controller

--- Core/Runtime/Game/Behaviours/Pawns/Controllers/FirstPersonController.cs
+++ Core/Runtime/Game/Behaviours/Pawns/Controllers/FirstPersonController.cs
@@ -1,4 +1,23 @@
-﻿namespace Espionage.Engine
+﻿using UnityEngine;
+
+namespace Espionage.Engine
 {
-	public class FirstPersonController : PawnController { }
+	public class FirstPersonController : PawnController
+	{
+		protected override void OnAwake()
+		{
+			Controller = GetComponent<CharacterController>();
+		}
+
+		public CharacterController Controller { get; private set; }
+
+		public override void Simulate( Client client )
+		{
+			var input = client.Input;
+
+			var wishDir = Pawn.EyeRot * Vector3.forward * input.Forward + Pawn.EyeRot * Vector3.left * input.Horizontal;
+
+			Controller.Move( wishDir + Vector3.down * 20 );
+		}
+	}
 }
