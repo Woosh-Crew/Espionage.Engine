129b884199b06066ed806926f82c9c4e33b733b4
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-19 10:45:10 +1100

    Sway Updates

--- Core/Runtime/Game/Behaviours/Viewmodel/Components/Sway/SimpleSway.cs
+++ Core/Runtime/Game/Behaviours/Viewmodel/Components/Sway/SimpleSway.cs
@@ -4,20 +4,16 @@ namespace Espionage.Engine.Viewmodels
 {
 	public sealed class SimpleSway : Behaviour, Viewmodel.IEffect
 	{
-		private Vector2 _lastDelta;
 		private Quaternion _lastSwayRot;
 		private Vector3 _lastSwayPos;
 
 		public void PostCameraSetup( ref ITripod.Setup setup )
 		{
-			// I have no clue wtf this does, but apparently me from the past did
-			_lastDelta = new Vector2( Input.GetAxisRaw( "Mouse X" ), Input.GetAxisRaw( "Mouse Y" ) );
-
-			var mouse = _lastDelta;
+			var mouse = new Vector2( Input.GetAxisRaw( "Mouse X" ), Input.GetAxisRaw( "Mouse Y" ) );
 			mouse *= multiplier;
 
 			var trans = transform;
-			_lastSwayRot = Quaternion.Slerp( _lastSwayRot, Quaternion.Euler( mouse.y, -mouse.x, mouse.x ), 6 * Time.deltaTime );
+			_lastSwayRot = Quaternion.Slerp( _lastSwayRot, Quaternion.Euler( mouse.y, mouse.x, mouse.x ), 6 * Time.deltaTime );
 			_lastSwayPos = Vector3.Lerp( _lastSwayPos, transform.localRotation * Vector3.up * mouse.y + trans.localRotation * Vector3.left * mouse.x, 6 * Time.deltaTime );
 
 			trans.localRotation *= _lastSwayRot;
