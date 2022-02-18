7c42982f6b01ee1fda120001a0709f0e9213e6b1
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-18 13:52:35 +1100

    Add Easing class

---
+++ Utility/Runtime/Easing.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Espionage.Engine
+{
+	public static class Easing
+	{
+		public static float Spring(float start, float end, float value)
+		{
+			value = Mathf.Clamp01(value);
+			value = (Mathf.Sin(value * Mathf.PI * (0.2f + 2.5f * value * value * value)) * Mathf.Pow(1f - value, 2.2f) + value) * (1f + (1.2f * (1f - value)));
+			return start + (end - start) * value;
+		}
+	}
+}
