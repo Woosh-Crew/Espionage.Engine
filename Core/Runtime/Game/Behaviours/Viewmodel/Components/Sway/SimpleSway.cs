3c329277c441103e422e5a10e7b02a038e720073
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-19 10:53:46 +1100

    Updates to Sway again, Reenable offset

--- Core/Runtime/Game/Behaviours/Viewmodel/Components/Sway/SimpleSway.cs
+++ Core/Runtime/Game/Behaviours/Viewmodel/Components/Sway/SimpleSway.cs
@@ -16,17 +16,22 @@ namespace Espionage.Engine.Viewmodels
 			_lastSwayRot = Quaternion.Slerp( _lastSwayRot, Quaternion.Euler( new Vector3( mouse.y, mouse.x, mouse.x ) * rotationMultiplier ), rotationDamping * Time.deltaTime );
 			_lastSwayPos = Vector3.Lerp( _lastSwayPos, transform.localRotation * Vector3.up * mouse.y + trans.localRotation * Vector3.left * mouse.x, offsetDamping * Time.deltaTime );
 
+			// For some reason local rotation..
+			// makes it stutter hard? Makes no sense
 			trans.rotation *= _lastSwayRot;
-			// trans.localPosition += _lastSwayPos;
+			trans.position += _lastSwayPos.normalized * offsetMultiplier;
 		}
 
 		// Fields
 
-		[SerializeField]
+		[Header( "Rotation" ), SerializeField]
 		private float rotationMultiplier = 1;
 
 		[SerializeField]
 		private float rotationDamping = 4;
+
+		[Header( "Offset" ), SerializeField]
+		private float offsetMultiplier = 1;
 
 		[SerializeField]
 		private float offsetDamping = 4;
