ca2c97b8bb1419120cecd888953cf0a52a01d735
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-19 10:20:02 +1100

    Set the objects layer to viewmodel, so it renders infront of everything on awake

--- Core/Runtime/Game/Behaviours/Viewmodel/Viewmodel.cs
+++ Core/Runtime/Game/Behaviours/Viewmodel/Viewmodel.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 namespace Espionage.Engine
 {
-    public class Viewmodel : Behaviour
-    {
-	    public static List<Viewmodel> All { get; } = new();
+	public sealed class Viewmodel : Behaviour
+	{
+		public static List<Viewmodel> All { get; } = new();
 
-	    protected override void OnAwake()
-	    {
-		    All.Add( this );
-	    }
+		protected override void OnAwake()
+		{
+			All.Add( this );
 
-	    protected override void OnDelete()
-	    {
-			All.Remove( this );		    
-	    }
+			foreach ( var render in GetComponentsInChildren<Renderer>() )
+			{
+				render.gameObject.layer = LayerMask.GetMask( "Viewmodel" );
+			}
+		}
 
-	    public void PostCameraSetup( ref ITripod.Setup setup )
-	    {
-		    var trans = transform;
-		    trans.localPosition = setup.Position;
-		    trans.localRotation = setup.Rotation;
-	    }
-    }
+		protected override void OnDelete()
+		{
+			All.Remove( this );
+		}
+
+		public void PostCameraSetup( ref ITripod.Setup setup )
+		{
+			var trans = transform;
+			trans.localPosition = setup.Position;
+			trans.localRotation = setup.Rotation;
+		}
+	}
 }
