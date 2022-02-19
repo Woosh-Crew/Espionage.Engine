466e9284e6eb49d92ddbfc56d673361bb03c68cb
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-19 15:26:07 +1100

    Create BasicOffset

---
+++ Core/Runtime/Game/Behaviours/Viewmodel/Components/Offsets/BasicOffset.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Espionage.Engine.Viewmodels
+{
+    public class BasicOffset : Behaviour, Viewmodel.IEffect
+    {
+	    public void PostCameraSetup( ref ITripod.Setup camSetup )
+	    {
+		    var trans = transform;
+		    trans.position += ( trans.rotation * Vector3.left * offset.x ) + (trans.rotation * Vector3.up *  offset.y ) + (trans.rotation * Vector3.forward * offset.z);
+	    }
+
+	    // Fields
+	    
+	    [SerializeField]
+	    private Vector3 offset;
+    }
+}
