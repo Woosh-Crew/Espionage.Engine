757033ac3521e3e604a28dfadf939099d4b0e0cf
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-19 11:08:33 +1100

    Update Sway to use Easing function

--- Core/Runtime/Game/Behaviours/Viewmodel/Components/Sway/SimpleSway.cs
+++ Core/Runtime/Game/Behaviours/Viewmodel/Components/Sway/SimpleSway.cs
@@ -15,8 +15,8 @@ namespace Espionage.Engine.Viewmodels
 			var trans = transform;
 			_lastSwayRot = Quaternion.Slerp( _lastSwayRot, Quaternion.Euler( new Vector3( mouse.y * rotationMultiplier.x, mouse.x * rotationMultiplier.y, mouse.x * rotationMultiplier.z ) ), rotationDamping * Time.deltaTime );
 
-			_lastSwayPos = Vector3.Lerp( _lastSwayPos, transform.localRotation * Vector3.up * mouse.y * offsetMultiplier.y +
-			                                           trans.localRotation * Vector3.left * mouse.x * offsetMultiplier.x, offsetDamping * Time.deltaTime );
+			_lastSwayPos = Easing.Linear( _lastSwayPos, transform.localRotation * Vector3.up * mouse.y * offsetMultiplier.y +
+			                                            trans.localRotation * Vector3.left * mouse.x * offsetMultiplier.x, offsetDamping * Time.deltaTime );
 
 			// For some reason local rotation..
 			// makes it stutter hard? Makes no sense
