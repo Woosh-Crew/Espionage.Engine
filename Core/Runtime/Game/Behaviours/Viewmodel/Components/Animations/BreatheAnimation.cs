839743808871e712fcdea70c12e4bfb062c64c7e
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-19 11:31:08 +1100

    Make BreatheAnimation Work

--- Core/Runtime/Game/Behaviours/Viewmodel/Components/Animations/BreatheAnimation.cs
+++ Core/Runtime/Game/Behaviours/Viewmodel/Components/Animations/BreatheAnimation.cs
@@ -10,20 +10,18 @@ namespace Espionage.Engine.Viewmodels
 
 		public void PostCameraSetup( ref ITripod.Setup setup )
 		{
-			// Only do breathe if low energy
-			_lastEnergyBreatheScale = _lastEnergyBreatheScale.LerpTo( energy.Amount.Remap( 95, 0, 0, 3.4f ), 25 * Time.deltaTime );
+			const float breatheSpeed = 0.6f;
+			const float breatheIntensity = 0.25f;
 
-			var breatheSpeed = 0.6f * _lastEnergyBreatheScale.x;
-			var breatheIntensity = 0.25f * _lastEnergyBreatheScale.z;
-
-			_breatheBobDelta += (Time.deltaTime) * breatheSpeed;
+			_breatheBobDelta += Time.deltaTime * breatheSpeed;
 
 			var breatheUp = MathF.Cos( _breatheBobDelta * 1f ) * 1.3f * breatheIntensity;
 			var breatheLeft = MathF.Sin( _breatheBobDelta * 0.5f ) * 0.8f * breatheIntensity;
 			var breatheForward = MathF.Cos( _breatheBobDelta * 0.5f ) * 0.5f * breatheIntensity;
 
-			transform.position += Rotation.Left * breatheLeft + Rotation.Up * breatheUp;;
-			transform.rotation *= Rotation.From( breatheUp * _lastEnergyBreatheScale.x, breatheLeft * _lastEnergyBreatheScale.y, breatheForward * _lastEnergyBreatheScale.z );
+			var trans = transform;
+			trans.position += trans.rotation * Vector3.left * breatheLeft + trans.rotation * Vector3.up * breatheUp;
+			transform.rotation *= Quaternion.Euler( breatheUp * _lastEnergyBreatheScale.x, breatheLeft * _lastEnergyBreatheScale.y, breatheForward * _lastEnergyBreatheScale.z );
 		}
 	}
 }
