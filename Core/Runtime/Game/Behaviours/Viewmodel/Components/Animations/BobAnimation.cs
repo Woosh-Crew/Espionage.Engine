edbd1c4977780097b5df453d1a8ed99bbde575f6
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-19 12:05:02 +1100

    Updates to Bob

--- Core/Runtime/Game/Behaviours/Viewmodel/Components/Animations/BobAnimation.cs
+++ Core/Runtime/Game/Behaviours/Viewmodel/Components/Animations/BobAnimation.cs
@@ -16,16 +16,16 @@ namespace Espionage.Engine.Pickups.Viewmodels
 		public void PostCameraSetup( ref ITripod.Setup camSetup )
 		{
 			//Set our velocity to the vector between our last position and this position
-			var position = transform.position;
+			var position = Local.Pawn.transform.position;
 
 			Velocity = (position - _lastPosition) / Time.deltaTime;
 			_lastPosition = position;
 
-			var speed = Mathf.InverseLerp( 0, 1, Velocity.magnitude );
+			var speed = Mathf.InverseLerp( 0, speedInverse, Velocity.magnitude );
 
 			_dampedSpeed = Mathf.Lerp( _dampedSpeed, speed, 2 * Time.deltaTime );
 			_walkBobScale = Mathf.Lerp( _walkBobScale, speed, 10 * Time.deltaTime );
-			_walkBobDelta += Time.deltaTime * 15.0f * _walkBobScale;
+			_walkBobDelta += Time.deltaTime * bobSpeedScale * _walkBobScale;
 
 			// Waves
 			_lastWalkBob.x = MathF.Sin( _walkBobDelta * 0.7f ) * 0.6f;
@@ -39,5 +39,13 @@ namespace Espionage.Engine.Pickups.Viewmodels
 			trans.position += trans.rotation * Vector3.up * _lastWalkBob.z + trans.rotation * Vector3.left * _lastWalkBob.y * 1.25f;
 			transform.rotation *= Quaternion.Euler( _lastWalkBob.z * 2, _lastWalkBob.y * 4, _lastWalkBob.x * 4 );
 		}
+
+		// Fields
+
+		[SerializeField]
+		private float speedInverse = 0.5f;
+
+		[SerializeField]
+		private float bobSpeedScale = 15f;
 	}
 }
