92f48181cb349547d3437142da8f63ccff37e187
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-23 22:33:56 +1100

    Create Function and Function Attribute

---
+++ Utility/Runtime/Reflection/Function/Attribute/FunctionAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace Espionage.Engine
+{
+	/// <summary>
+	/// Properties are variables that are changeable by the editor.
+	/// </summary>
+	[AttributeUsage( AttributeTargets.Method, Inherited = true, AllowMultiple = false )]
+	public sealed class FunctionAttribute : Attribute
+	{
+		public string Name { get; set; }
+		public string Title { get; set; }
+		public string Group { get; set; }
+		public string Help { get; set; }
+
+		public FunctionAttribute() { }
+
+		public FunctionAttribute( string name )
+		{
+			Name = name;
+		}
+
+		public Function CreateRecord( Library library, MethodInfo info )
+		{
+			return new Function( library, info )
+			{
+				Name = Name,
+				Help = Help,
+				Title = Title,
+				Group = Group,
+			};
+		}
+	}
+}
---
+++ Utility/Runtime/Reflection/Function/Function.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Espionage.Engine.Components;
+
+namespace Espionage.Engine
+{
+	public sealed class Function : IMember
+	{ 
+		public MethodInfo Info { get; }
+		public Components<Function> Components { get; }
+		
+		internal Function( Library owner, MethodInfo info )
+		{
+			ClassInfo = owner;
+			Info = info;
+
+			Name = info.Name;
+			Title = info.Name;
+
+			// Components
+			Components = new Components<Function>( this );
+
+			// This is really expensive (6ms)...
+			// Get Components attached to type
+			foreach ( var item in Info.GetCustomAttributes() )
+			{
+				if ( item is IComponent<Function> property )
+				{
+					Components.Add( property );
+				}
+			}
+		}
+		
+		public string Name { get; set; }
+		public string Title { get; set; }
+		public string Group { get; set; }
+		public string Help { get; set; }
+		
+		private Library ClassInfo { get; }
+	}
+}
