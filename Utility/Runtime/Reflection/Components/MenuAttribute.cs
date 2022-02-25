e2b7b37a3c59fa74b4d70e2dabcb90891e56af2c
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-25 14:59:00 +1100

    Updates

---
+++ Utility/Runtime/Reflection/Components/MenuAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Espionage.Engine.Components;
+
+namespace Espionage.Engine
+{
+	public class MenuAttribute : Attribute, IComponent<Function>
+	{
+		public void OnAttached( Function item ) {  }
+	}
+}
