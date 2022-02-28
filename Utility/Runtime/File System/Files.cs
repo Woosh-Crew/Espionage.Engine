76f1a5d9642f29e01d8a6efef738b086ca46aeb5
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-03-01 09:53:28 +1100

    Add Documentation to File System

--- Utility/Runtime/File System/Files.cs
+++ Utility/Runtime/File System/Files.cs
@@ -1,14 +1,24 @@
-﻿using System.Diagnostics.Tracing;
-using System.IO;
+﻿using System.IO;
 using System.Text;
+using UnityEngine;
 
 namespace Espionage.Engine
 {
+	/// <summary>
+	/// Files, is Espionage.Engines File System.
+	/// All Saving, Loading, ETC, is local to the games
+	/// data storage.
+	/// </summary>
 	public static class Files
 	{
-		// Just Give us the Raw Data.
+		/// <summary>
+		/// Just gives us the raw data
+		/// from a file at a path
+		/// </summary>
 		private static byte[] Load( string path )
 		{
+			path = Path.Combine( Application.dataPath, path );
+
 			if ( !File.Exists( path ) )
 			{
 				throw new FileNotFoundException( "File doesn't Exist" );
@@ -17,13 +27,25 @@ namespace Espionage.Engine
 			return File.ReadAllBytes( path );
 		}
 
+		/// <summary>
+		/// Saves a string value to a path,
+		/// it will overwrite if the file at that
+		/// path already exists.
+		/// </summary>
 		private static void Save( string text, string path )
 		{
 			Save( Encoding.UTF8.GetBytes( text ), path );
 		}
 
+		/// <summary>
+		/// Saves a byte buffer to a path,
+		/// it will overwrite if the file at that
+		/// path already exists.
+		/// </summary>
 		private static void Save( byte[] data, string path )
 		{
+			path = Path.Combine( Application.dataPath, path );
+
 			if ( !Directory.Exists( path ) )
 			{
 				Directory.CreateDirectory( path );
@@ -33,9 +55,13 @@ namespace Espionage.Engine
 			stream.Write( data );
 		}
 
-
+		/// <summary>
+		/// Opens a FileStream to the designated path.
+		/// </summary>
 		private static FileStream Read( string path )
 		{
+			path = Path.Combine( Application.dataPath, path );
+
 			return new FileStream( path, FileMode.Open, FileAccess.Read );
 		}
 	}
