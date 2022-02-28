f588d4e3e3845cd8eaea3280545e08afa5606d99
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-03-01 09:48:38 +1100

    Initial work on File System

---
+++ Core/Runtime/Game/Activators/Output.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Espionage.Engine.Activators
+{
+	public struct Output
+	{
+		public Function Target { get; }
+		
+		public void Invoke()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Utility/Runtime/File System/Files.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.Tracing;
+using System.IO;
+using System.Text;
+
+namespace Espionage.Engine
+{
+	public static class Files
+	{
+		// Just Give us the Raw Data.
+		private static byte[] Load( string path )
+		{
+			if ( !File.Exists( path ) )
+			{
+				throw new FileNotFoundException( "File doesn't Exist" );
+			}
+
+			return File.ReadAllBytes( path );
+		}
+
+		private static void Save( string text, string path )
+		{
+			Save( Encoding.UTF8.GetBytes( text ), path );
+		}
+
+		private static void Save( byte[] data, string path )
+		{
+			if ( !Directory.Exists( path ) )
+			{
+				Directory.CreateDirectory( path );
+			}
+
+			using var stream = File.Create( path );
+			stream.Write( data );
+		}
+
+
+		private static FileStream Read( string path )
+		{
+			return new FileStream( path, FileMode.Open, FileAccess.Read );
+		}
+	}
+}
