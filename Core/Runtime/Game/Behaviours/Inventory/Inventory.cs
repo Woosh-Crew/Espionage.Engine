86cec8ae901789994e09c0c3bac0558ca57a7bbd
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-28 14:13:35 +1100

    Add Inventory and Add Pickup

--- Core/Runtime/Game/Behaviours/Inventory/Inventory.cs
+++ Core/Runtime/Game/Behaviours/Inventory/Inventory.cs
@@ -1,7 +1,61 @@
-﻿namespace Espionage.Engine
+﻿using System;
+using System.Collections.Generic;
+using Espionage.Engine.Components;
+
+namespace Espionage.Engine
 {
-	public class Inventory : Behaviour
+	public class Inventory : Behaviour, IComponent<Pawn>, IDatabase<Pickup>
 	{
-		
+		public IEnumerable<Pickup> All => Items;
+		public Pawn Pawn { get; private set; }
+
+		void IComponent<Pawn>.OnAttached( Pawn item )
+		{
+			Pawn = item;
+		}
+
+		private List<Pickup> Items { get; set; }
+
+		public virtual void Add( Pickup item )
+		{
+			// If we dont own this and if we cant carry it, ignore
+			if ( Contains( item ) || item.Owner is not null )
+			{
+				Debugging.Log.Info( $"Can't pickup item {item}" );
+				return;
+			}
+
+			Items.Add( item );
+			item.OnPickup( Pawn );
+		}
+
+		public virtual void Drop( Pickup item )
+		{
+			if ( !Contains( item ) )
+			{
+				throw new Exception( $"Item [{item.ClassInfo.Name}] isn't in Inventory" );
+			}
+
+			item.OnDrop( Pawn );
+			Items.Remove( item );
+		}
+
+		public virtual void Remove( Pickup item )
+		{
+			if ( item is null )
+			{
+				throw new NullReferenceException( "Pickup was NULL" );
+			}
+
+			Items.Remove( item );
+		}
+
+		public void Clear() { }
+
+
+		public virtual bool Contains( Pickup item )
+		{
+			return Items.Contains( item );
+		}
 	}
 }
---
+++ Core/Runtime/Game/Behaviours/Inventory/Pickup/Pickup.cs
@@ -0,0 +1,19 @@
+﻿namespace Espionage.Engine
+{
+	public class Pickup : Behaviour
+	{
+		public Pawn Owner { get; private set; }
+		
+		public virtual bool Droppable => true;
+
+		public virtual void OnPickup( Pawn carrier )
+		{
+			Owner = carrier;
+		}
+
+		public virtual void OnDrop( Pawn dropper )
+		{
+			Owner = null;
+		}
+	}
+}
