2c0071775e39f3fad0a7b42346efd30bb905c12e
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-03-01 11:28:38 +1100

    Add IO Input Attribute

---
+++ Core/Runtime/Game/Activators/Attributes/InputAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Espionage.Engine.Components;
+
+namespace Espionage.Engine.Activators
+{
+	public class InputAttribute : Attribute, IComponent<Function>
+	{
+		public void OnAttached( Function item ) {  }
+	}
+}
