a84211b3a8bc8723079a59787383ac2ce68cc576
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-03-04 10:34:10 +1100

    Update ThirdPersonTripod.cs to acount for changes

--- Core/Runtime/Game/Tripods/Behaviours/TripodBehaviour.cs
+++ Core/Runtime/Game/Tripods/Behaviours/TripodBehaviour.cs
@@ -2,14 +2,13 @@
 
 namespace Espionage.Engine.Tripods
 {
+	[Group( "Tripods" )]
 	public class TripodBehaviour : Behaviour, ITripod, IControls
 	{
-		public Tripod Active { get; private set; }
+		public ITripod Active { get; set; }
 
 		protected override void OnAwake()
 		{
-			base.OnAwake();
-
 			Active = Library.Database.Create<Tripod>( tripod );
 		}
 
@@ -27,19 +26,26 @@ namespace Espionage.Engine.Tripods
 
 		void ITripod.Build( ref ITripod.Setup setup )
 		{
-			((ITripod)Active)?.Build( ref setup );
+			// Set the Viewer here. The Tripod
+			// Should be responsible for removing it.
+			setup.Viewer = visuals;
+
+			Active?.Build( ref setup );
 		}
 
 		// Controls
 
 		void IControls.Build( ref IControls.Setup setup )
 		{
-			((IControls)Active)?.Build( ref setup );
+			(Active as IControls)?.Build( ref setup );
 		}
 
 		// Fields
 
 		[SerializeField]
 		private string tripod;
+
+		[SerializeField]
+		private Transform visuals;
 	}
 }
--- Core/Runtime/Game/Tripods/ThirdPersonTripod.cs
+++ Core/Runtime/Game/Tripods/ThirdPersonTripod.cs
@@ -28,6 +28,10 @@ namespace Espionage.Engine.Tripods
 			var relativeOffset = camSetup.Rotation * Vector3.right * Offset.x + camSetup.Rotation * Vector3.up * Offset.y;
 
 			camSetup.Position = Local.Pawn.EyePos + relativeOffset + camSetup.Rotation * Vector3.back * ((ray ? hitInfo.distance : Distance) - Padding);
+
+			// We're not looking through the eyes of anything
+			// So don't disable its visuals...
+			camSetup.Viewer = null;
 		}
 
 		protected override void OnBuildControls( ref IControls.Setup setup )
@@ -36,14 +40,14 @@ namespace Espionage.Engine.Tripods
 			setup.ViewAngles.x = Mathf.Clamp( setup.ViewAngles.x, -60, 88 );
 		}
 
-		// Fields
+		// Properties
 
 		[Property] public Vector2 Offset { get; set; }
 
-		[Property] public float Smoothing { get; set; }
+		[Property] public float Smoothing { get; set; } = 20;
 
-		[Property] public float Distance { get; set; }
+		[Property] public float Distance { get; set; } = 5;
 
-		[Property] public float Padding { get; set; }
+		[Property] public float Padding { get; set; } = 0.1f;
 	}
 }
