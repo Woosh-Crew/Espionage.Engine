53cfc7ebb725bff2c776b4fd565c4bbb1ea6fcc1
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-13 15:07:41 +1100

    Add DOF, SSR, Motion Blur and Chromatic Aberration to Options

--- Runtime/Utility/Options/Options.cs
+++ Runtime/Utility/Options/Options.cs
@@ -75,7 +75,7 @@ namespace Espionage.Engine
 
 		// AO
 
-		[Option, Terminal, Property( "graphics.enable_ao", true ), Title( "Enable AO" ), Group( "Graphics" )]
+		[Option, Terminal, Property( "graphics.enable_ao", true ), Title( "Enable Ambient Occlusion" ), Group( "Graphics" )]
 		public static bool EnableAO { get; set; }
 
 		// Bloom
@@ -85,6 +85,33 @@ namespace Espionage.Engine
 
 		[Option, Terminal, Property( "graphics.bloom_quality", false ), Title( "Bloom Quality" ), Group( "Graphics" )]
 		public static bool FastBloom { get; set; }
+
+		// SSR
+
+		[Option, Terminal, Property( "graphics.enable_ssr", true ), Title( "Enable Screen Space Reflections" ), Group( "Graphics" )]
+		public static bool EnableSSR { get; set; }
+
+		[Option, Terminal, Property( "graphics.ssr_quality", ScreenSpaceReflectionPreset.Medium ), Title( "Screen Space Reflections Quality" ), Group( "Graphics" )]
+		public static ScreenSpaceReflectionPreset QualitySSR { get; set; }
+
+		// Motion Blur
+
+		[Option, Terminal, Property( "graphics.enable_motion_blur", true ), Title( "Enable Motion Blur" ), Group( "Graphics" )]
+		public static bool EnableMotionBlur { get; set; }
+
+		// Depth of Field
+
+		[Option, Terminal, Property( "graphics.enable_dof", true ), Title( "Enable Depth of Field" ), Group( "Graphics" )]
+		public static bool EnableDOF { get; set; }
+
+		// Chromatic Aberration
+
+		[Option, Terminal, Property( "graphics.enable_ca", true ), Title( "Enable Chromatic Aberration" ), Group( "Graphics" )]
+		public static bool EnableChromaticAberration { get; set; }
+
+		[Option, Terminal, Property( "graphics.fast_ca", true ), Title( "Fast Chromatic Aberration" ), Group( "Graphics" )]
+		public static bool FastChromaticAberration { get; set; }
+
 
 		// Assigning
 
@@ -128,12 +155,51 @@ namespace Espionage.Engine
 			if ( profile.HasSettings<Bloom>() )
 			{
 				var bloom = profile.GetSetting<Bloom>();
-				bloom.active = EnableAO;
+				bloom.active = EnableBloom;
 
 				// Do shit only if we're active
 				if ( bloom.active )
 				{
 					bloom.fastMode.Override( FastBloom );
+				}
+			}
+
+			// Screen Space Reflections
+			if ( profile.HasSettings<ScreenSpaceReflections>() )
+			{
+				var ssr = profile.GetSetting<ScreenSpaceReflections>();
+				ssr.active = EnableSSR;
+
+				// Do shit only if we're active
+				if ( ssr.active )
+				{
+					ssr.preset.Override( QualitySSR );
+				}
+			}
+
+			// Motion Blur
+			if ( profile.HasSettings<MotionBlur>() )
+			{
+				var blur = profile.GetSetting<MotionBlur>();
+				blur.active = EnableMotionBlur;
+			}
+
+			// Depth of Field
+			if ( profile.HasSettings<DepthOfField>() )
+			{
+				var dof = profile.GetSetting<DepthOfField>();
+				dof.active = EnableDOF;
+			}
+
+			// Chromatic Aberration
+			if ( profile.HasSettings<ChromaticAberration>() )
+			{
+				var ca = profile.GetSetting<ChromaticAberration>();
+				ca.active = EnableChromaticAberration;
+
+				if ( ca.active )
+				{
+					ca.fastMode.Override( FastChromaticAberration );
 				}
 			}
 		}
