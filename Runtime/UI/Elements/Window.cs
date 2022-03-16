f87420c9e73e1ecead1c2f14392f497d55821644
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-16 13:45:40 +1100

    Initial Work on Terminal UI

---
+++ Runtime/UI/Elements/Window.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace Espionage.Engine.UI	
+{
+	public class Window : VisualElement
+	{
+		public new class UxmlFactory : UxmlFactory<Window, UxmlTraits> {}
+		public new class UxmlTraits : VisualElement.UxmlTraits { }
+
+		public Window()
+		{
+		}
+	}
+}
