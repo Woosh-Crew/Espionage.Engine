b5f0d0b7dc8283d24e35f889ece48494fe8f94d0
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-16 11:30:31 +1100

    Initial work on Asset bundle UI Binder

--- Runtime/Resources/UI/Binders/AssetBundleUIBinder.cs
+++ Runtime/Resources/UI/Binders/AssetBundleUIBinder.cs
@@ -1,17 +1,58 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Espionage.Engine.Resources.Binders
 {
 	public class AssetBundleUIBinder : UI.Binder
 	{
 		public override string Identifier { get; }
-		
+
+		public AssetBundleUIBinder( string path )
+		{
+			Identifier = path;
+		}
+
+		//
+		// Resource
+		//
+
+		private AssetBundleCreateRequest _bundleRequestOperation;
+		private AsyncOperation _sceneLoadOperation;
+
+		private AssetBundle _bundle;
+
 		public override void Load( Action onLoad = null )
 		{
+			// Load Bundle
+			_bundleRequestOperation = AssetBundle.LoadFromFileAsync( Identifier );
+			_bundleRequestOperation.completed += ( _ ) =>
+			{
+				// When we've finished loading the asset
+				// bundle, go onto loading the scene itself
+				_bundle = _bundleRequestOperation.assetBundle;
+				Debugging.Log.Info( "Finished Loading Bundle" );
+
+				// Load the scene by getting all scene
+				// paths from a bundle, and getting the first index
+				var asset = _bundle.LoadAllAssetsAsync<VisualTreeAsset>();
+				_sceneLoadOperation.completed += ( _ ) =>
+				{
+					// We've finished loading the scene.
+					Debugging.Log.Info( "Finished Loading UI" );
+					onLoad?.Invoke();
+				};
+			};
 		}
 
 		public override void Unload( Action onUnload = null )
 		{
+			var request = _bundle.UnloadAsync( true );
+			request.completed += ( e ) =>
+			{
+				Debugging.Log.Info( "Finished Unloading Bundle" );
+				onUnload?.Invoke();
+			};
 		}
 	}
 }
--- Runtime/Resources/UI/UI.cs
+++ Runtime/Resources/UI/UI.cs
@@ -5,6 +5,34 @@ namespace Espionage.Engine.Resources
 	[Group( "User Interfaces" ), Title( "User Interface" ), Path( "ui", "assets://Interfaces/" )]
 	public partial class UI : Resource
 	{
+		/// <summary>
+		/// Trys to find the UI by path. If it couldn't find the UI in the database,
+		/// it'll create a new reference to that UI for use later.
+		/// </summary>
+		public static UI Find( string path )
+		{
+			path = Files.Pathing.Absolute( path );
+
+			// Use the Database Map if we have it
+			if ( Database[path] != null )
+			{
+				return Database[path] as UI;
+			}
+
+			if ( !Files.Pathing.Exists( path ) )
+			{
+				Debugging.Log.Error( $"UI Path [{Files.Pathing.Absolute( path )}], couldn't be found." );
+				return null;
+			}
+
+			var file = Files.Serialization.Load<File>( path );
+			return new( file.Binder );
+		}
+
+		//
+		// Instance
+		//
+
 		public override string Identifier { get; }
 		private Binder Provider { get; }
 
