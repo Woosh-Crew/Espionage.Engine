02c438ea3f312b7b3841194be1813c63f3f2a199
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-20 23:10:20 +1100

    Fix bug where guntuck would sometimes go through walls

--- Runtime/Core/Entity/Viewmodel/Components/Gameplay/Guntuck.cs
+++ Runtime/Core/Entity/Viewmodel/Components/Gameplay/Guntuck.cs
@@ -8,22 +8,19 @@ namespace Espionage.Engine.Viewmodels
 
 		public override void PostCameraSetup( ref Tripod.Setup setup )
 		{
-			var start = setup.Position;
-			var direction = setup.Rotation * Vector3.forward;
+			var start = end.position + end.rotation * Vector3.back * Vector3.Distance( end.position, setup.Position );
+			var direction = end.rotation * Vector3.forward;
 			var distance = Vector3.Distance( setup.Position, end.position );
 
 			var ray = new Ray( start, direction );
-
 			var hit = Physics.Raycast( ray, out var info, distance, ~LayerMask.GetMask( "Viewmodel", "Pawn" ) );
-			
+
 			var offset = hit ? info.distance - distance : 0;
-			
+
 			_dampedOffset = _dampedOffset.LerpTo( -offset, 8 * Time.deltaTime );
 
 			transform.position += setup.Rotation * Vector3.back * _dampedOffset;
 			transform.position += setup.Rotation * Vector3.down * _dampedOffset / 2;
-
-			Debug.DrawLine( ray.origin, ray.origin + ray.direction * distance, Color.green );
 		}
 
 
