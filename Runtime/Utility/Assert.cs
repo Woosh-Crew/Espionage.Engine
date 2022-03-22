66f30a52b48d418408dcd473c8c180a9f18f8d7e
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-23 00:42:30 +1100

    Add conditional's to Assert.cs

--- Runtime/Utility/Assert.cs
+++ Runtime/Utility/Assert.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace Espionage.Engine
@@ -9,32 +10,53 @@ namespace Espionage.Engine
 	/// </summary>
 	public static class Assert
 	{
-		public static void IsEmpty( ICollection collection, [CallerMemberName] string caller = null )
+		[Conditional( "DEBUG" )]
+		public static void IsEmpty( ICollection collection, string message = null, [CallerLineNumber] int caller = default )
 		{
 			if ( collection.Count == 0 )
-				Fail( $"Collection was Empty! {caller}" );
+			{
+				Fail( $"Collection was Empty! [{caller}]" );
+			}
 		}
-		
-		public static void IsNull<T>( T item, [CallerMemberName] string caller = null )
+
+		[Conditional( "DEBUG" )]
+		public static void IsNull<T>( T item, string message = null, [CallerLineNumber] int caller = default )
 		{
 			if ( item == null )
-				Fail( $"Item was NULL! {caller}" );
+			{
+				Fail( $"Item was NULL! [{caller}]" );
+			}
 		}
-		
+
 		// Bool
 
-		public static void IsTrue( bool item, [CallerMemberName] string caller = null )
+		[Conditional( "DEBUG" )]
+		public static void IsEqual<T>( T item, T comparison, string message = null, [CallerLineNumber] int caller = default )
 		{
-			if ( item )
-				Fail( $"Bool was True! {caller}" );
+			if ( item.Equals( comparison ) )
+			{
+				Fail( $"{typeof( T ).Name} was equals comparison! [{caller}]" );
+			}
 		}
 
-		public static void IsFalse( bool item, [CallerMemberName] string caller = null )
+		[Conditional( "DEBUG" )]
+		public static void IsTrue( bool item, string message = null, [CallerLineNumber] int caller = default )
+		{
+			if ( item )
+			{
+				Fail( $"Bool was True! [{caller}]" );
+			}
+		}
+
+		[Conditional( "DEBUG" )]
+		public static void IsFalse( bool item, string message = null, [CallerLineNumber] int caller = default )
 		{
 			if ( item == false )
-				Fail( $"Bool was False! {caller}" );
+			{
+				Fail( $"Bool was False! [{caller}]" );
+			}
 		}
-		
+
 		// Utility
 
 		private static void Fail( string message )
