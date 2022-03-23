f049ec09362e6da87445139a2af65e8a56480e8c
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-23 12:49:14 +1100

    Add ILFW file

---
+++ Runtime/Resources/Map/File/ILFW.cs
@@ -0,0 +1,10 @@
+﻿using Espionage.Engine.Resources.Binders;
+
+namespace Espionage.Engine.Resources.Formats
+{
+	[Title( "Intruder Level File" ), File( Extension = "ilfw" )]
+	public sealed class ILFW : Map.File
+	{
+		public override Map.Binder Binder => new AssetBundleMapProvider( Source.FullName );
+	}
+}
