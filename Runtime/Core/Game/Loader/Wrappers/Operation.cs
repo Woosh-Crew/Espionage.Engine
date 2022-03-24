10d9174e840da8c107c852b835fcd31e1fe149a8
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-24 22:13:53 +1100

    Add AssetBundleFile and Operation

---
+++ Runtime/Core/Game/Loader/Wrappers/Operation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Espionage.Engine
+{
+	public class Operation : ILoadable
+	{
+		private readonly Action<Action> _operation;
+		private readonly string _text;
+
+		public static ILoadable Create( Action<Action> loaded, string text )
+		{
+			return new Operation( loaded, text );
+		}
+
+		private Operation( Action<Action> loaded, string text )
+		{
+			_text = text;
+			_operation = loaded;
+		}
+
+		// Loadable
+
+		float ILoadable.Progress => 0;
+		string ILoadable.Text => _text;
+
+		void ILoadable.Load( Action loaded )
+		{
+			_operation.Invoke( loaded );
+		}
+	}
+}
---
+++ Runtime/Resources/Map/File/AssetBundleFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Espionage.Engine.Resources.Binders;
+using UnityEngine;
+
+namespace Espionage.Engine.Resources.Formats
+{
+	public class AssetBundleFile : Map.File
+	{
+		public AssetBundle Bundle { get; private set; }
+
+		public override void Load( Action loaded )
+		{
+			// Load Bundle
+			var request = AssetBundle.LoadFromFileAsync( Info.FullName );
+			request.completed += _ =>
+			{
+				Bundle = request.assetBundle;
+				Binder = new AssetBundleMapProvider( Bundle );
+
+				Dev.Log.Info( $"Finished Loading Asset Bundle [{Info.Name}]" );
+				loaded.Invoke();
+			};
+		}
+
+		public override void Unload( Action finished )
+		{
+			Bundle.UnloadAsync( true ).completed += _ =>
+			{
+				Dev.Log.Info( $"Finished Unloading Asset Bundle [{Info.Name}]" );
+				finished.Invoke();
+			};
+		}
+	}
+}
