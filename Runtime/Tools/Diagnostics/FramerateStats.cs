9ada5f87bf60ddbe33f7ac09dbc8f201e887bc7c
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-29 01:38:13 +1100

    Make Framerate Stats its own window

---
+++ Runtime/Tools/Diagnostics/FramerateStats.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using ImGuiNET;
+using ImPlotNET;
+using UnityEngine;
+
+namespace Espionage.Engine.Tools
+{
+	public class FramerateStats : Window
+	{
+		private readonly Queue<float> _fps = new( 20 );
+		private int _lastFrame;
+		private TimeSince _timeSinceUpdate = 0;
+
+		private int _low;
+		private int _top;
+
+		public override void OnLayout()
+		{
+			// Using a queue is stupid, but i dont care
+
+			if ( _timeSinceUpdate > 0.1f )
+			{
+				_timeSinceUpdate = 0;
+
+				var value = 1 / Time.smoothDeltaTime;
+
+				_fps.Enqueue( value );
+				_lastFrame = (int)value;
+
+				if ( value > _top )
+				{
+					_top = (int)value;
+				}
+
+				if ( value < _low || _low == 0 )
+				{
+					_low = (int)value;
+				}
+
+				if ( _fps.Count > 20 )
+				{
+					_fps.Dequeue();
+				}
+			}
+
+			ImGui.Text( $"FPS: {_lastFrame}" );
+
+			if ( _fps.Count <= 0 )
+			{
+				return;
+			}
+
+			var values = _fps.ToArray();
+
+			ImGui.SetNextItemWidth( 256 );
+			ImGui.PlotLines( string.Empty, ref values[0], _fps.Count - 1, 0, string.Empty, _low, _top, new( 0, 32 ) );
+		}
+	}
+}
--- Runtime/Tools/Stats.cs
+++ Runtime/Tools/Stats.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ImGuiNET;
+using ImPlotNET;
 using UnityEngine;
 
 namespace Espionage.Engine.Tools
@@ -11,56 +12,6 @@ namespace Espionage.Engine.Tools
 		{
 			ImGui.Text( $"Time: {(int)Time.time}" );
 			ImGui.Text( $"Time Scale: {Time.timeScale}" );
-		}
-	}
-
-	public class FrameStats : Overlay
-	{
-		private readonly Queue<float> _fps = new( 20 );
-		private int _lastFrame;
-		private TimeSince _timeSinceUpdate = 0;
-
-		private int _low;
-		private int _top;
-
-		public override void OnLayout()
-		{
-			// Using a queue is stupid, but i dont care
-
-			if ( _timeSinceUpdate > 0.1f )
-			{
-				_timeSinceUpdate = 0;
-
-				var value = 1 / Time.smoothDeltaTime;
-
-				_fps.Enqueue( value );
-				_lastFrame = (int)value;
-
-				if ( value > _top )
-				{
-					_top = (int)value;
-				}
-
-				if ( value < _low || _low == 0 )
-				{
-					_low = (int)value;
-				}
-
-				if ( _fps.Count > 20 )
-				{
-					_fps.Dequeue();
-				}
-			}
-
-			ImGui.Text( $"FPS: {_lastFrame}" );
-
-			if ( _fps.Count > 0 )
-			{
-				ImGui.SetNextItemWidth( 128 );
-				var values = _fps.ToArray();
-				ImGui.PlotLines( string.Empty, ref values[0], _fps.Count - 1, 0, string.Empty, _low, _top, new( 0, 32 ) );
-			}
-
 		}
 	}
 
