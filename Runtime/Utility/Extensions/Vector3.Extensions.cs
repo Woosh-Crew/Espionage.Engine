9527d7fe9c33a34a7704de9b74cad1eb6ac2dcc9
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-28 11:03:25 +1100

    Port Flip Vector3 from Netscape

--- Runtime/Utility/Extensions/Vector3.Extensions.cs
+++ Runtime/Utility/Extensions/Vector3.Extensions.cs
@@ -4,20 +4,41 @@ namespace Espionage.Engine
 {
 	public static class Vector3Extensions
 	{
+		// With
+
 		public static Vector3 WithX( this Vector3 vector, float value )
 		{
-			return new Vector3( value, vector.y, vector.z );
+			return new( value, vector.y, vector.z );
 		}
 
 		public static Vector3 WithY( this Vector3 vector, float value )
 		{
-			return new Vector3( vector.x, value, vector.z );
+			return new( vector.x, value, vector.z );
 		}
 
 		public static Vector3 WithZ( this Vector3 vector, float value )
 		{
-			return new Vector3( vector.x, vector.y, value );
+			return new( vector.x, vector.y, value );
 		}
+
+		// Flip
+
+		public static Vector3 FlipX( this Vector3 v )
+		{
+			return new( -v.x, v.y, v.z );
+		}
+
+		public static Vector3 FlipY( this Vector3 v )
+		{
+			return new( v.x, -v.y, v.z );
+		}
+
+		public static Vector3 FlipZ( this Vector3 v )
+		{
+			return new( v.x, v.y, -v.z );
+		}
+
+		// Lerp
 
 		public static Vector3 LerpTo( this Vector3 input, Vector3 b, float t )
 		{
