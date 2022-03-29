0329d3d3768f24c99fa0ad00b2229648aac53817
Geeknerd1337 <geeknerd1337@gmail.com>
2022-03-29 10:43:06 -0500

    Document Holdable

---
+++ Runtime/Core/Entity/Pickup/Fire Arms/Firearm_SO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Espionage.Engine
+{
+	/// <summary>
+	/// The scriptable object for firearms.
+	/// </summary>
+	[CreateAssetMenu(fileName = "Firearm", menuName = "Weapons/Firearms", order = 1)]
+	public class Firearm_SO : ScriptableObject
+	{
+		
+	}
+}
--- Runtime/Core/Entity/Pickup/Holdable.cs
+++ Runtime/Core/Entity/Pickup/Holdable.cs
@@ -4,37 +4,41 @@ namespace Espionage.Engine
 {
 	public abstract class Holdable : Pickup, ISimulated
 	{
+		/// <summary>The given viewmodel for a holdable</summary>
 		public Viewmodel Viewmodel { get; set; }
 
 		public virtual void Simulate( Client cl ) { }
 		public virtual void PostCameraSetup( ref Tripod.Setup setup ) { }
 
-		//
-		// Deploy
-		//
 
+		/// <summary>If a given holdable is deploying</summary>
 		public bool IsDeploying { get; set; }
 
+		/// <summary>If a given weapon can be deployed</summary>
 		public virtual bool CanDeploy()
 		{
 			return true;
 		}
 
+		/// <summary>Deploys a given holdable</summary>
 		public virtual void Deploy() { }
+
+		/// <summary>Call back for when holdable deployed</summary>
 		protected virtual void OnDeployed() { }
 
-		//
-		// Holster
-		//
-
+		/// <summary>If a given holdable is currently holstering</summary>
 		public bool IsHolstering { get; set; }
 
+		/// <summary>If a given holdable can be holstered</summary>
 		public virtual bool CanHolster()
 		{
 			return true;
 		}
 
+		/// <summary>Holsters the given holdable</summary>
 		public virtual void Holster( bool dropped = false ) { }
+
+		/// <summary>Call back for when a given holdable is holstered</summary>
 		protected virtual void OnHolstered() { }
 	}
 }
