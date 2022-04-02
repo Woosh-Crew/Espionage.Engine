671f15aba2c409f7397fd41c60f37ae46ba6e975
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-04-02 21:09:51 +1100

    Add Pushing Actor Component, Which using the mswheel allows you to push entities, such as doors.

---
+++ Runtime/Core/Entity/Components/Gameplay/Pushing.cs
@@ -0,0 +1,27 @@
+﻿namespace Espionage.Engine
+{
+	public class Pushing : Component<Actor>, ISimulated
+	{
+		public void Simulate( Client client )
+		{
+			var wheel = client.Input.Mouse.Wheel;
+
+			if ( wheel != 0 )
+			{
+				Push( wheel );
+			}
+		}
+
+		public virtual void Push( float mouseDelta, float size = 0 )
+		{
+			var ray = Trace.Ray( Entity.EyePos, Entity.EyeRot.Forward(), 0.9f ).Ignore( "Pawn" );
+			var entity = ray.Run<IPushable>() ?? ray.Radius( size ).Run<IPushable>();
+			entity?.OnPush( mouseDelta, Entity );
+		}
+	}
+
+	public interface IPushable
+	{
+		void OnPush( float delta, Pawn pawn );
+	}
+}
