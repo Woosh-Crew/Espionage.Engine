ea892cd1a7132ad3b65fc47251f121bc123ea7e8
Geeknerd1337 <geeknerd1337@gmail.com>
2022-04-08 10:54:49 -0500

    Update Tester.cs

--- Runtime/Utility/Debug/Overlay/Tester.cs
+++ Runtime/Utility/Debug/Overlay/Tester.cs
@@ -6,13 +6,9 @@ namespace Espionage.Engine
 {
     public class Tester : MonoBehaviour
     {
-		public Mesh mesh;
-    public Material material;
     public void Update() {
-		Debug.Log("HMM");
-        // will make the mesh appear in the scene at origin position
-        //Graphics.DrawMesh(mesh, transform.position, Quaternion.identity, material, 0);
-		Overlay.DrawSphere(transform.position,2f,1f,Color.blue);
+
+		Overlay.DrawSphere(transform.position,2f,0f,Color.blue);
     }
     }
 }
