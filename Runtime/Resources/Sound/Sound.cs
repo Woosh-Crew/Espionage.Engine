192d2f7dbec58719d929dcb247a0183b44d24fb1
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-05-01 21:50:13 +1000

    Add Sound asset type

--- Runtime/Resources/Sound/Sound.cs
+++ Runtime/Resources/Sound/Sound.cs
@@ -1,21 +1,62 @@
 ﻿using System.IO;
+using UnityEngine;
 
 namespace Espionage.Engine.Resources
 {
 	[Group( "Sounds" ), Path( "sounds", "assets://Sounds" ), File( Extension = "usnd" )]
 	public class Sound : Data
 	{
+		public bool UI { get; set; }
+		public float Distance { get; set; }
+
+		public Vector2 Volume { get; set; }
+		public Vector2 Pitch { get; set; }
+
+		public string[] Sounds { get; set; }
+
 		// Asset Compiling & Loading
 		// --------------------------------------------------------------------------------------- //
-		
+
 		protected override void OnCompile( BinaryWriter writer )
 		{
-			base.OnCompile( writer );
+			// Generic
+
+			writer.Write( UI );
+			writer.Write( Distance );
+
+			// Volume
+
+			writer.Write( Volume.x );
+			writer.Write( Volume.y );
+
+			// Pitch
+
+			writer.Write( Pitch.x );
+			writer.Write( Pitch.y );
+
+			// Sounds
+
+			writer.Write( Sounds.Length );
+
+			for ( var i = 0; i < Sounds.Length; i++ )
+			{
+				writer.Write( Sounds[i] );
+			}
 		}
 
 		protected override void OnLoad( BinaryReader reader )
 		{
-			base.OnLoad( reader );
+			UI = reader.ReadBoolean();
+			Distance = reader.ReadSingle();
+			Volume = new( reader.ReadSingle(), reader.ReadSingle() );
+			Pitch = new( reader.ReadSingle(), reader.ReadSingle() );
+
+			Sounds = new string[reader.ReadInt32()];
+
+			for ( var i = 0; i < Sounds.Length; i++ )
+			{
+				Sounds[i] = reader.ReadString();
+			}
 		}
 	}
 }
